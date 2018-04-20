3fb5148c41a800c874e1a496b96526584cb44f92
Stanley Yu <stanley98yu@gmail.com>
2018-04-20 00:10:30 -0400

    Removed potentiometer and increased clock time.

--- BopIt/BopIt.ino
+++ BopIt/BopIt.ino
@@ -10,10 +10,10 @@
 
 
 // Global constants: pin numbers for LEDs and buttons
-const int rled = 2, rbtn = 8;  // Red: 2, 8 
+const int rled = 2, rbtn = 8;  // Red: 2, 8 BUTTON
 const int gled = 3, pot = A5;  // Green: 3, 10 POTENTIOMETER
-const int yled = 4, pr = A4;  // Yellow: 4, 12 PHOTORESISTOR
-const int startbtn = 13; // Start Btn: 13
+const int bled = 4, sw = A4;  // Blue: 4, 12 SWITCH
+const int startbtn = 9; // Start Btn: 9
 const int buzzer = 5;
 
 // ******************** SETUP ********************
@@ -22,16 +22,14 @@ void setup() {
   //Set leds and buzzer to outputs
   pinMode(rled, OUTPUT);
   pinMode(gled, OUTPUT);
-  pinMode(yled, OUTPUT);
+  pinMode(bled, OUTPUT);
   pinMode(buzzer, OUTPUT);
 
   //Set buttons to inputs
   pinMode(rbtn, INPUT);
   pinMode(pot, INPUT);
-  pinMode(pr, INPUT);
+  pinMode(sw, INPUT);
   pinMode(startbtn, INPUT);
-
-  Serial.begin(9000);
 }
 
 // ******************** MAIN LOOP ********************
@@ -41,15 +39,13 @@ void loop() {
   int seq[30]; // Sequence of computer moves
   int usr_in = 0; // User input
   int turn = 0; // Player turn
-  int clock_time = 1000; // Speed of flashing lights
+  int clock_time = 500; // Speed of flashing lights
   boolean start = false;
-  Serial.print(analogRead(pot));
 
   // Game not in progress, initialize sequence
   if (start == false) {
     usr_in = 0;
     turn = 0;
-    clock_time = 1000;
 
     // Random sequence of moves per game.
     randomSeed(analogRead(0));
@@ -58,9 +54,9 @@ void loop() {
   }
 
   // Button status
-  int rbtn_st = digitalRead(rbtn);
-  int gbtn_st = digitalRead(pot);
-  int ybtn_st = digitalRead(pr);
+  //int rbtn_st = digitalRead(rbtn);
+  //int gbtn_st = digitalRead(pot);
+  //int ybtn_st = digitalRead(sw);
   int startbtn_st = digitalRead(startbtn);
 
   // Push start button to begin.
@@ -81,21 +77,21 @@ void loop() {
         seqFlash(2, clock_time);
       else if (seq[i] == 3) // Green
         seqFlash(3, clock_time);
-      else if (seq[i] == 4) // Yellow
+      else if (seq[i] == 4) // Blue
         seqFlash(4, clock_time);
     }
 
     // Player's turn to match pattern
     for (int k = 0; k < turn; k++) {
-      int originalpot = analogRead(pot);
       while (usr_in == 0) {
         if (digitalRead(rbtn) == HIGH) {
           buttonFlash(2);
           usr_in = 2;
-        } else if (analogRead(pot) != originalpot) {
+        } else if (analogRead(pot) > 50) {
           buttonFlash(3);
           usr_in = 3;
-        } else if (analogRead(pr) < 200) {
+          delay(10);
+        } else if (digitalRead(sw) == 1) {
           buttonFlash(4);
           usr_in = 4;
         } else {
@@ -105,17 +101,25 @@ void loop() {
 
       if (usr_in != seq[k]) { // Player is incorrect
         start = false;
-        tone(buzzer, 800);
-        delay(3000);
-        noTone(buzzer);
-        digitalWrite(seq[k], HIGH);
-        delay(3000);
-        digitalWrite(seq[k], LOW);
+        for(int i = 0; i < 1; i++) {
+            tone(buzzer, 800);
+            delay(200);
+            noTone(buzzer);
+        }
       } else { // Player is correct
         usr_in = 0;
-        clock_time -= 10; // Reduce clock time.
-        delay(500);
+        clock_time -= 15; // Reduce clock time.
+        delay(100);
       }
+    }
+
+    if(turn > 30) { // Player wins the game.
+        for(int i = 0; i < 3; i++) {
+            tone(buzzer, 800);
+            delay(200);
+            noTone(buzzer);
+            delay(10);
+        }        
     }
   }
 }
@@ -127,6 +131,7 @@ void buttonFlash(int color) { // Flash button colour
   digitalWrite(color, HIGH);
   delay(200);
   digitalWrite(color, LOW);
+  delay(200);
 }
 
 void seqFlash(int pin, int clock_time) { // Flash in sequence
